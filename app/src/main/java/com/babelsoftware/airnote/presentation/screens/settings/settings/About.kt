b71abd45801546e97e997d66765254c06685da95
Mustafa Burak Özcan <m06.burakozcan@proton.me>
2025-07-27 22:11:18 +0300

    refactor(About): Reference version update indicating changes made in AI

--- app/src/main/java/com/babelsoftware/airnote/presentation/screens/settings/settings/About.kt
+++ app/src/main/java/com/babelsoftware/airnote/presentation/screens/settings/settings/About.kt
@@ -122,7 +122,7 @@ fun AboutScreen(
                 SettingsBox(
                     settingsViewModel = settingsViewModel,
                     title = "AirNote AI Version",
-                    description = "v1.1.3 build v0.9.0",
+                    description = "v1.1.4 build v0.9.0",
                     icon = IconResource.Vector(Icons.Rounded.AutoAwesome),
                     actionType = ActionType.TEXT,
                     radius = shapeManager(
