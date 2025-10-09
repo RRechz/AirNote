33b7b3552b76f583f9bce2ebbd0b6bea04260d12
Mustafa Burak Özcan <m06.burakozcan@proton.me>
2025-10-10 00:28:14 +0300

    Feat(AiModels): Experimental ai models removed

--- app/src/main/java/com/babelsoftware/airnote/data/repository/AiModels.kt
+++ app/src/main/java/com/babelsoftware/airnote/data/repository/AiModels.kt
@@ -6,15 +6,7 @@ object GeminiModels {
         "gemini-pro-vision",
         "gemini-1.5-flash-latest",
         "gemini-1.5-pro-latest",
-        "gemini-pro-latest",
         "gemini-2.0-flash-001",
         "gemini-2.0-pro-001",
-        // ---> Experimental models
-        "embedding-001",
-        "text-multilingual-embedding-002",
-        "text-embedding-004",
-        "text-embedding-ada-002",
-        "text-embedding-ada-001"
-        // <---
     )
 }
