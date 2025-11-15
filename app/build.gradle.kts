433b260f352a3b09afa06c7b3d56f27d4113f5cc
Mustafa Burak Özcan <m06.burakozcan@proton.me>
2025-11-15 21:53:52 +0300

    Feat(source): Source update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 1
-        versionName = "v1.6"
+        versionName = "v1.6.1"
         vectorDrawables {
             useSupportLibrary = true
         }
